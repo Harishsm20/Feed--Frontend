8ae1c9ebc2882afb37a2e2910fd639fa0e44baf8
Jaisurya <jaisuryau@onlightenmedia.onmicrosoft.com>
2024-07-24 14:35:51 +0530

    Login and Signup Pages corrected

--- src/Component/Authentication/Signup.tsx
+++ src/Component/Authentication/Signup.tsx
@@ -1,7 +1,7 @@
-import img1 from '../../assets/Images/LoginImg/login1.png';
-import img2 from '../../assets/Images/LoginImg/login2.png';
-import img3 from '../../assets/Images/LoginImg/login3.png';
-import img4 from '../../assets/Images/LoginImg/login4.png';
+import img1 from '../../assets/Images/LoginImg/signup1.png';
+import img2 from '../../assets/Images/LoginImg/signup2.png';
+import img3 from '../../assets/Images/LoginImg/signup3.png';
+import img4 from '../../assets/Images/LoginImg/signup4.png';
 import apple from '../../assets/Images/IconSvg/apple.png';
 import facebook from '../../assets/Images/IconSvg/facebook.png';
 import github from '../../assets/Images/IconSvg/github.png';
@@ -92,31 +92,30 @@ const Signup = () => {
               {/* <img src={google} alt='Google' className='w-8 h-8 grayscale hover:grayscale-0' /> */}
               <svg xmlns="http://www.w3.org/2000/svg" className='w-8 h-8 grayscale hover:grayscale-0 hover:-translate-y-2 transition-all duration-500' preserveAspectRatio="xMidYMid" viewBox="0 0 256 262" id="google"><path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path><path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"></path><path fill="#FBBC05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"></path><path fill="#EB4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path></svg>
               <img src={apple} alt='Apple' className='w-8 h-8 grayscale hover:grayscale-0 hover:-translate-y-2  transition-all duration-500' />
+              <img src={microsoft} alt='GitHub' className='w-8 h-8 grayscale hover:grayscale-0 hover:-translate-y-2 transition-all duration-500' />
               <img src={facebook} alt='Facebook' className='w-8 h-8 grayscale hover:grayscale-0 hover:-translate-y-2 transition-all duration-500' />
               <img src={github} alt='GitHub' className='w-8 h-8 grayscale hover:grayscale-0 hover:-translate-y-2 transition-all duration-500'  />
-              <img src={microsoft} alt='GitHub' className='w-8 h-8 grayscale hover:grayscale-0 hover:-translate-y-2 transition-all duration-500' />
             </div>
           </div>
-          <p className='text-white text-center mt-2'>Already have an account? <a href='/login' className='underline' >Login</a></p>
+          <p className='text-white text-center mt-2'> <a href='/login' className='underline' >Already have an account?</a></p>
         </div>
-        <div className='grid grid-cols-2 grid-rows-2 gap-3 size-6/12 mt-8'>
+        <div className='grid grid-cols-2 grid-rows-2 gap-3 w-1/2 mt-12 mb-9'>
           <div className='row-span-2'>
-          <img src={img1} alt='Image 1' className='rounded-lg hover:-translate-y-2 transition-all duration-500 h-full' />    
+          <img src={img1} alt='Image 1' className='rounded-lg hover:-translate-y-2 transition-all duration-500 h-full w-full' />    
           </div>
           
           <div className='row-span-1'>
-          <img src={img2} alt='Image 1' className='rounded-lg hover:-translate-y-2 transition-all duration-500 h-full' />    
+          <img src={img2} alt='Image 1' className='rounded-lg hover:-translate-y-2 transition-all duration-500 h-full w-full' />    
           </div>
           
           <div className='row-span-2'>
-          <img src={img3} alt='Image 1' className='rounded-lg hover:-translate-y-2 transition-all duration-500 h-full' />    
+          <img src={img3} alt='Image 1' className='rounded-lg hover:-translate-y-2 transition-all duration-500 h-full w-full' />    
           </div>
           
           <div className='row-span-1'>
-          <img src={img4} alt='Image 1' className='rounded-lg hover:-translate-y-2 transition-all duration-500 h-full' />    
+          <img src={img4} alt='Image 1' className='rounded-lg hover:-translate-y-2 transition-all duration-500 h-full w-full' />    
           </div>
         </div>
-        
       </div>
     </div>
   );
