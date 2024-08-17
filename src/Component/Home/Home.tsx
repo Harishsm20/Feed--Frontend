a9ec8a4c70d19900df4d379587575e0c35982f04
Harishsm20 <harishsm865@gmail.com>
2024-08-17 21:56:49 +0530

    Profile Component Updated

---
+++ src/Component/Home/Home.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../NavBar/NavBar"
+import CustomSlider from '../CustomSlider/CustomSlider'
+
+const Home = () => {
+  return (
+    <div>
+        <NavBar />
+        <CustomSlider />
+    </div>
+  )
+}
+
+export default Home
--- src/Component/Profile/Profile.tsx
+++ src/Component/Profile/Profile.tsx
@@ -1,12 +1,34 @@
 // import loadereffect from '../loadereffect';
 import NavBar from '../NavBar/NavBar'
-import CustomSilder from '../CustomSlider/CustomSlider';
 
 const Profile = () => {
   return (
     <div>
       <NavBar />
-      <CustomSilder />
+      <div className='mt-2 flex flex-col bg-[#b5ab58] '>
+
+        {/*Profile pic*/}
+        <div className=' flex justify-center mt-2'>
+          <div className='border-2 rounded-full w-20 h-20 text-center mb-4'>
+          Profile Picture
+          </div>
+        </div>
+
+        {/*Followers and Following contents */}
+        <div className='grid grid-cols-3 justify-items-center mt-6 mb-5'>
+            <div className=''>
+              Posts
+            </div>
+            <div className=''>
+              followers
+            </div>
+
+            <div className=''>
+              following
+            </div>
+
+        </div>
+      </div>
     </div>
   )
 }
